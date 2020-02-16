02af81f642043ed333fdde8c024acaaece2110be
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-02-16 00:46:00 -0800

    first Javascript project
    simple draw plate with food

---
+++ breakfast.js
@@ -0,0 +1,28 @@
+background(186, 145, 20); // wooden table
+ellipse(200, 200, 350, 350); // plate
+ellipse(200, 200, 300, 300); 
+
+//egg
+ellipse(250,150, 150,100);
+fill(247, 255, 0);
+ellipse(250,150,50,50);
+
+//cheese
+triangle(200,250,300,200,250,300);
+ellipse(230, 260, 10,10);
+ellipse(260, 260, 10,10);
+ellipse(260, 230, 10,10);
+
+//sausage
+noFill();
+stroke(219, 127, 150);
+strokeWeight(20);
+arc(150, 100, 50, 50, 1, 180);  
+arc(100, 150, 50, 50, 1, 180); 
+
+//bread
+stroke(71, 40, 23);
+strokeWeight(2);
+fill(230, 184, 67);
+rect(120,200, 60,80);
+ellipse(150, 200, 80,30);
